e7ee61c86ad5ef494c046c9b9cd5b399cd70d278
Felix Mannhardt <felix.mannhardt@sintef.no>
2019-01-27 00:49:11 +0100

    Added CI, Added evaluation test code

---
+++ R/evaluation.R
@@ -0,0 +1,33 @@
+#' @import reticulate
+evaluation_token_replay <- function(eventlog,
+                                  petrinet,
+                                  initial_marking,
+                                  final_marking,
+                                  parameters = param_activity_key(bupaR::activity_id(eventlog)),
+                                  variant = variant_token_based(),
+                                  convert = TRUE) {
+  pm4py_evaluation <- import("pm4py.evaluation.factory", convert = convert)
+
+  py_pn <- as_py_value(petrinet)
+  py_log <- as_py_value(eventlog)
+
+  # bugfix for https://github.com/pm4py/pm4py-source/issues/42
+  if ("pm4py:param:activity_key" %in% names(parameters)) {
+    parameters <- c(parameters, list("activity_key" = parameters[["pm4py:param:activity_key"]]))
+  }
+
+  #bug in PM4PY
+
+  m <- pm4py_evaluation$apply(log = py_log,
+                              net = py_pn,
+                              initial_marking = as_pm4py_marking(initial_marking, py_pn),
+                              final_marking = as_pm4py_marking(final_marking, py_pn),
+                              parameters = parameters,
+                              variant = variant)
+  m
+}
+
+variant_token_based <- function() {
+  pm4py$evaluation$factory$TOKEN_BASED
+}
+
