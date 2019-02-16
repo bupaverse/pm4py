a6a6602b71e115839f6f7e7a3c5981ed01ec2e4f
Felix Mannhardt <felix.mannhardt@sintef.no>
2019-02-16 17:51:46 +0100

    Prepare for CRAN using release v1.0.24 of PM4PY

--- R/version.R
+++ R/version.R
@@ -1,11 +1,13 @@
 # Whether the version is a package or a development version
-PM4PY_DEVELOPMENT=TRUE
+PM4PY_DEVELOPMENT=FALSE
 
 # PM4PY version, may be any pip identifier allowed, either:
 #  pm4py==1.0.21
 # or a Github link to a specific branch/tag/hash:
 #  https://github.com/pm4py/pm4py-source/archive/develop.zip
-PM4PY_VERSION="https://github.com/pm4py/pm4py-source/archive/9f76dd7e2dfe8df7bfaa05b82539458cccd7247a.zip"
+#PM4PY_VERSION="https://github.com/pm4py/pm4py-source/archive/9f76dd7e2dfe8df7bfaa05b82539458cccd7247a.zip"
+PM4PY_VERSION="pm4py==1.0.24"
 
 # PM4PY dependencies for development versions
-PM4PY_DEPENDENCIES="https://raw.githubusercontent.com/pm4py/pm4py-source/9f76dd7e2dfe8df7bfaa05b82539458cccd7247a/requirements.txt"
+#PM4PY_DEPENDENCIES="https://raw.githubusercontent.com/pm4py/pm4py-source/9f76dd7e2dfe8df7bfaa05b82539458cccd7247a/requirements.txt"
+PM4PY_DEPENDENCIES="https://raw.githubusercontent.com/pm4py/pm4py-source/1.0.24/requirements.txt"
