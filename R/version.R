267c7d81c021dccab15fc1043ff597f94cf1bf6b
Felix Mannhardt <felix.mannhardt@sintef.no>
2019-07-07 14:40:38 +0200

    Update to PM4Py v1.1.19

--- R/version.R
+++ R/version.R
@@ -6,8 +6,8 @@ PM4PY_DEVELOPMENT=FALSE
 # or a Github link to a specific branch/tag/hash:
 #  https://github.com/pm4py/pm4py-source/archive/develop.zip
 #PM4PY_VERSION="https://github.com/pm4py/pm4py-source/archive/9f76dd7e2dfe8df7bfaa05b82539458cccd7247a.zip"
-PM4PY_VERSION="pm4py==1.1.14"
+PM4PY_VERSION="pm4py==1.1.19"
 
 # PM4PY dependencies for development versions
 #PM4PY_DEPENDENCIES="https://raw.githubusercontent.com/pm4py/pm4py-source/9f76dd7e2dfe8df7bfaa05b82539458cccd7247a/requirements.txt"
-PM4PY_DEPENDENCIES="https://raw.githubusercontent.com/pm4py/pm4py-source/1.1.14/requirements.txt"
+PM4PY_DEPENDENCIES="https://raw.githubusercontent.com/pm4py/pm4py-source/1.1.19/requirements.txt"
